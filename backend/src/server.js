3cfdcbbbfbff46525d36a7f7691ddf11103af0fa
Manav Gupta <manavgupta457@gmail.com>
2025-10-01 01:14:24 +0530

    Final Backend Changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,20 +2,23 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 5001
-connectDB();
 
 //middleware
 app.use(express.json())
+app.use(rateLimiter)
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(port, () => {
-    console.log("Server started at port 5001");
-});
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log("Server started at port 5001");
+    });
+})
 
 //mongodb+srv://manavgupta:manavgupta@cluster0.hoxkdcd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
